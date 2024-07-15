c37614a9b73435ff5cecb34ee0993253f572d242
Christine Hoang <christinehoangt@gmail.com>
2024-07-15 17:42:28 -0500

    text box added to app.jsx

--- src/components/TextBox.jsx
+++ src/components/TextBox.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const TextBox = () => {
-  return <div>TextBox</div>;
+  return (
+    <div>
+      <input placeholder="Enter your text here..." />
+    </div>
+  );
 };
 
 export default TextBox;
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
+// import '../styles/index.scss'
+import TextBox from "../components/TextBox";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="Home">
+      <TextBox />
+    </div>
+  );
 };
 
 export default Home;
