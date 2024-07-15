a13fe3a853ed4fa70647955bbb94f0ed11e1240b
Christine Hoang <christinehoangt@gmail.com>
2024-07-15 17:57:11 -0500

    i added basic form fields to Forms.jsx and pulled it into Home.jsx

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Form = () => {
-  return <div>Form</div>;
+  return (
+    <div>
+      <form action="">
+        <label htmlFor="fname">First name:</label><br />
+        <input type="text" id="fname" name="fname" defaultValue="John" /><br />
+        <label htmlFor="lname">Last name:</label><br />
+        <input type="text" id="lname" name="lname" defaultValue="Doe" /><br /><br />
+        <input type="submit" value="Submit" />
+      </form> 
+    </div>
+  );
 };
 
 export default Form;
--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 // import '../styles/index.scss'
 import TextBox from "../components/TextBox";
+import Form from "../components/Form";
 
 const Home = () => {
   return (
     <div className="Home">
+      <Form/>
       <TextBox />
     </div>
   );
