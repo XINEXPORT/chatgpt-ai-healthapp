0cf89a6b26bda6d2699b79672d0789379d877b24
Christine Hoang <christinehoangt@gmail.com>
2024-07-16 15:10:15 -0500

    dropdown list of ethnicities and additonal demograpgics fields added

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -12,6 +12,42 @@ const Form = () => {
         <br />
         <input type="text" id="lname" name="lname" defaultValue="Doe" />
         <br />
+        <label htmlFor="age">Age:</label>
+        <br />
+        <input type="number" id="age" name="age" />
+        <br />
+        <label htmlFor="weight">Weight:</label>
+        <br />
+        <input type="number" id="weight" name="weight" />
+        <br />
+        <label htmlFor="ethnicity">Ethnicity:</label>
+        <br />
+        <select id="ethnicity" name="ethnicity">
+          <option value="Indigenous American/Native Alaskan">Indigenous American/Native Alaskan</option>
+          <option value="Black African">Black African</option>
+          <option value="Black or African American">Black or African American</option>
+          <option value="East Asian">East Asian</option>
+          <option value="Southeast Asian">Southeast Asian</option>
+          <option value="South Asian">South Asian</option>
+          <option value="Native Hawaiian or Pacific Islander">Native Hawaiian or Pacific Islander</option>
+          <option value="Latine/Hispanic/Latinx or Spanish origin">Latine/Hispanic/Latinx or Spanish origin</option>
+          <option value="Middle Eastern or North African">Middle Eastern or North African</option>
+          <option value="White or European Descent">White or European Descent</option>
+          <option value="Prefer not to say">Prefer not to say</option>
+          <option value="Prefer to describe">Prefer to describe</option>
+        </select>
+        <br />
+        <label>Sex:</label>
+        <br />
+        <input type="radio" id="male" name="sex" value="male" />
+        <label htmlFor="male">Male</label>
+        <br />
+        <input type="radio" id="female" name="sex" value="female" />
+        <label htmlFor="female">Female</label>
+        <br />
+        <input type="radio" id="other" name="sex" value="other" />
+        <label htmlFor="other">Other</label>
+        <br />
         <br />
         <input type="submit" value="Submit" />
       </form>
