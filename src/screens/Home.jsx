7de3eff4af08234495513dd2a20977e84c2ce3bf
htameez <htameez@smu.edu>
2024-07-16 19:22:09 -0500

    first commit

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,13 +1,14 @@
 import React from "react";
-// import '../styles/index.scss'
+//import 'styles/index.scss'
 import TextBox from "../components/TextBox";
 import Form from "../components/Form";
+import Button from 'react-bootstrap/Button';
 
 const Home = () => {
   return (
     <div className="Home">
-      <Form />
-      <TextBox />
+      <p>Welcome to the iPhone 15 layout example!</p>
+      <h1>Hello</h1>
     </div>
   );
 };
