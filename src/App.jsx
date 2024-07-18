5a1983af2cf3a52ea4697dadb3c12cf2ef64061d
Christine Hoang <christinehoangt@gmail.com>
2024-07-17 20:20:59 -0500

    completed a basic patient form. I also made pulled in the patient form in app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import Home from "./screens/Home.jsx";
 import Loading from "./screens/Loading.jsx";
+import PatientForm from "./screens/PatientForm.jsx";
 import "./index.scss";
 
 const App = () => {
@@ -9,7 +10,8 @@ const App = () => {
     <div id="iphone-15-frame">
       <div className="screen">
         <div className="content">
-          <Loading />
+          {/* <Loading /> */}
+          <PatientForm/>
         </div>
         <footer></footer>
       </div>
---
+++ src/screens/PatientForm.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Form from '../components/Form.jsx'
+
+const PatientForm = () => {
+  return <div>
+          <Form />  
+        </div>;
+};
+
+export default PatientForm;
