e98a9e04a56117e838e10f1ec2120d1593ee47e5
Christine Hoang <christinehoangt@gmail.com>
2024-07-21 21:46:56 -0500

    ran prettier and pulled from main

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -3,7 +3,8 @@ import { useNavigate } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "../index.scss";
 
-const API_URL = "https://api-inference.huggingface.co/models/openai-community/gpt2";
+const API_URL =
+  "https://api-inference.huggingface.co/models/openai-community/gpt2";
 const API_KEY = process.env.VITE_API_TOKEN;
 
 const Form = () => {
@@ -83,52 +84,120 @@ const Form = () => {
     <div className="container mt-5">
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
-          <label htmlFor="firstName" className="form-label text-white">First Name:</label>
-          <input type="text" className="form-control" id="firstName" name="firstName" value={firstName} onChange={handleFirstNameChange} />
+          <label htmlFor="firstName" className="form-label text-white">
+            First Name:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="firstName"
+            name="firstName"
+            value={firstName}
+            onChange={handleFirstNameChange}
+          />
         </div>
         <div className="mb-3">
-          <label htmlFor="lastName" className="form-label text-white">Last Name:</label>
-          <input type="text" className="form-control" id="lastName" name="lastName" value={lastName} onChange={handleLastNameChange} />
+          <label htmlFor="lastName" className="form-label text-white">
+            Last Name:
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            id="lastName"
+            name="lastName"
+            value={lastName}
+            onChange={handleLastNameChange}
+          />
         </div>
         <div className="mb-3">
-          <label htmlFor="age" className="form-label text-white">Age:</label>
-          <input type="number" className="form-control" id="age" name="age" value={age} onChange={handleAgeChange} />
+          <label htmlFor="age" className="form-label text-white">
+            Age:
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="age"
+            name="age"
+            value={age}
+            onChange={handleAgeChange}
+          />
         </div>
         <div className="mb-3">
-          <label htmlFor="weight" className="form-label text-white">Weight:</label>
-          <input type="number" className="form-control" id="weight" name="weight" value={weight} onChange={handleWeightChange} />
+          <label htmlFor="weight" className="form-label text-white">
+            Weight:
+          </label>
+          <input
+            type="number"
+            className="form-control"
+            id="weight"
+            name="weight"
+            value={weight}
+            onChange={handleWeightChange}
+          />
         </div>
         <div className="mb-3">
-          <label htmlFor="goal" className="form-label text-white">Goal:</label>
-          <select id="goal" name="goal" className="form-select" value={goal} onChange={handleGoalChange}>
+          <label htmlFor="goal" className="form-label text-white">
+            Goal:
+          </label>
+          <select
+            id="goal"
+            name="goal"
+            className="form-select"
+            value={goal}
+            onChange={handleGoalChange}
+          >
             <option value="">Select your goal</option>
             <option value="Lose 5lbs">Lose 5lbs</option>
             <option value="Lose 10lbs">Lose 10lbs</option>
             <option value="Lose 15lbs">Lose 15lbs</option>
             <option value="Lose 20lbs">Lose 20lbs</option>
-            <option value="Maintain current weight">Maintain current weight</option>
+            <option value="Maintain current weight">
+              Maintain current weight
+            </option>
           </select>
         </div>
         <div className="mb-3">
-          <label htmlFor="ethnicity" className="form-label text-white">Ethnicity:</label>
-          <select id="ethnicity" name="ethnicity" className="form-select" value={ethnicity} onChange={handleEthnicityChange}>
+          <label htmlFor="ethnicity" className="form-label text-white">
+            Ethnicity:
+          </label>
+          <select
+            id="ethnicity"
+            name="ethnicity"
+            className="form-select"
+            value={ethnicity}
+            onChange={handleEthnicityChange}
+          >
             <option value="">Select your ethnicity</option>
-            <option value="Indigenous American/Native Alaskan">Indigenous American/Native Alaskan</option>
+            <option value="Indigenous American/Native Alaskan">
+              Indigenous American/Native Alaskan
+            </option>
             <option value="Black African">Black African</option>
-            <option value="Black or African American">Black or African American</option>
+            <option value="Black or African American">
+              Black or African American
+            </option>
             <option value="East Asian">East Asian</option>
             <option value="Southeast Asian">Southeast Asian</option>
             <option value="South Asian">South Asian</option>
-            <option value="Native Hawaiian or Pacific Islander">Native Hawaiian or Pacific Islander</option>
-            <option value="Latine/Hispanic/Latinx or Spanish origin">Latine/Hispanic/Latinx or Spanish origin</option>
-            <option value="Middle Eastern or North African">Middle Eastern or North African</option>
-            <option value="White or European Descent">White or European Descent</option>
+            <option value="Native Hawaiian or Pacific Islander">
+              Native Hawaiian or Pacific Islander
+            </option>
+            <option value="Latine/Hispanic/Latinx or Spanish origin">
+              Latine/Hispanic/Latinx or Spanish origin
+            </option>
+            <option value="Middle Eastern or North African">
+              Middle Eastern or North African
+            </option>
+            <option value="White or European Descent">
+              White or European Descent
+            </option>
             <option value="Prefer not to say">Prefer not to say</option>
             <option value="Prefer to describe">Prefer to describe</option>
           </select>
         </div>
         {errorMessage && <div className="text-danger mb-3">{errorMessage}</div>}
-        <button type="submit" className="btn btn-primary">Submit</button>
+        <button type="submit" className="btn btn-primary">
+          Submit
+        </button>
       </form>
     </div>
   );
