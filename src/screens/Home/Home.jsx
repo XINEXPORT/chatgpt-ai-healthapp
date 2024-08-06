33fb394d4482bae0619de191cfacb08ea1a8ef55
Christine Hoang <christinehoangt@gmail.com>
2024-08-06 18:30:06 -0500

    Home.jsx exported project replaced with CareBuddy

--- src/screens/Home/Home.jsx
+++ src/screens/Home/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
   return (
     <div className={styles.Home}>
       <Helmet>
-        <title>exported project</title>
+        <title>CareBuddy</title>
       </Helmet>
       <div className={styles.homeContainer}>
         <div className={`styles.aboveMessageBox fadingContent ${isTyping ? 'fadeOut' : 'fadeIn'}`}>
